24c460a836acf55b104a7520ecc084566967b61d
Tikhon Slasten <slasten32@gmail.com>
2017-10-09 23:32:13 +1000

    Delete count of rows and columns from generated states

--- GenerateStates/GenerateStates.py
+++ GenerateStates/GenerateStates.py
@@ -10,16 +10,9 @@ with open(CSV_NAME) as csvfile:
     with open(TXT_NAME, 'w') as txtfile:
         print('{', file = txtfile)
 
-        row_count = 0
-        col_count = 0
-
         for row in reader:
-            row_count += 1
-            col_count = len(row)
-
             print('{', file = txtfile)
             print(','.join(row[1:len(row)]), file = txtfile)
             print('},', file = txtfile)
 
         print('}', file = txtfile)
-        print('[{0}][{1}]'.format(row_count, col_count), file = txtfile)
