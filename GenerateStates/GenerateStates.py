1ab0f6f6f4d4881d3e8575b33cb49744c4e8c10e
Tikhon Slasten <slasten32@gmail.com>
2017-10-15 18:27:16 +1000

    Change table generation

--- GenerateStates/GenerateStates.py
+++ GenerateStates/GenerateStates.py
@@ -5,17 +5,14 @@ CSV_NAME = 'States.csv'
 TXT_NAME = 'States.txt'
 
 path = os.path.dirname(os.path.realpath(__file__))
+tabs = ' ' * 12
 
 with open(path + os.sep + CSV_NAME) as csvfile:
     reader = csv.reader(csvfile, delimiter = ';')
     next(reader)
 
     with open(path + os.sep +TXT_NAME, 'w') as txtfile:
-        print('{', file = txtfile)
-
         for row in reader:
-            print('{', file = txtfile)
-            print(','.join(row[1:len(row)]), file = txtfile)
-            print('},', file = txtfile)
-
-        print('}', file = txtfile)
+            print(tabs + '{', file = txtfile)
+            print(tabs + ','.join(row[1:len(row)]), file = txtfile)
+            print(tabs + '},', file = txtfile)
